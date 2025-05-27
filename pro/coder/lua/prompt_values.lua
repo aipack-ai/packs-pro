a5cfff55f43419001523993353a828819973a300
Jeremy Chone <jeremy.chone@gmail.com>
2025-05-26 21:28:53 -0700

    . coder - v0.2.8

--- pro/coder/lua/prompt_values.lua
+++ pro/coder/lua/prompt_values.lua
@@ -12,8 +12,9 @@ local prompt_template = [[
 ```toml
 #!meta - parametric agent block
 
-# Relative to the workspace dir (the dir with the .aipack/). Can be an absolute path.
-# Pull in knowledge file content into context.
+# Paths or globs relative to the workspace directory (the directory containing `.aipack/`),
+# or absolute paths, including those starting with `~/` for home directories.
+# These will be referenced as "knowledge files".
 # knowledge_globs = ["path/to/knowledge/**/*.md", "core@doc/**/*.md", "pro@rust10x/guide/base/**/*.md"]
 
 # If not set, context_globs and working_globs won't be evaluated
@@ -31,15 +32,18 @@ context_globs = ["package.json", "src/**/*.*"]
 # working_concurrency = true
 # input_concurrency   = 6
 
-# Note: Make sure to update the `-exp` models with their latest
-model_aliases = {main = "gpt-4.1", mini = "gpt-4.1-mini", nano = "gpt-4.1-nano", claude = "claude-3-7-sonnet-latest", gpro = "gemini-2.5-pro-preview-05-06", flash = "gemini-2.5-flash-preview-04-17"}
+# Note: This will add/override the model_aliases defined in .aipack/config.toml and ~/.aipack-base/config.toml
+model_aliases = {gpt = "gpt-4.1", gpro = "gemini-2.5-pro-preview-05-06", claude = "claude-sonnet-4-20250514"}
+
+# Experimental flag to set the file content replace to search/replace when possible (can increase speed and decrease cost)
+# xp_file_content_mode = "search_replace_auto" # default "whole"
 
 # Set to true to write the files (otherwise, will show below the `====` )
 write_mode = false
 
 # It can be an alias name above, or model names like "o4-mini", "o4-mini-high".
 # If not set, the model defined in config.toml will be used.  
-model = "main"
+model = "gpt"
 
 # To see docs, type "Show Doc" and then press `r` in the aip terminal
 ```
