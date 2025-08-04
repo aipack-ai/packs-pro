c4a576145062127700788723a5ead40b82eed86e
Jeremy Chone <jeremy.chone@gmail.com>
2025-08-03 18:17:55 -0700

    . coder - udpate model_aliases to just give an example

--- pro/coder/lua/prompt_values.lua
+++ pro/coder/lua/prompt_values.lua
@@ -1,6 +1,5 @@
 local default_context_globs = nil
 
-
 local prompt_template = [[
 ```yaml
 #!meta (parametric prompt)
@@ -32,17 +31,10 @@ structure_globs:
 # working_concurrency: true
 # input_concurrency: 6
 
-# Note: This will add/override the model_aliases defined in .aipack/config.toml and ~/.aipack-base/config.toml
+# Note: This will add/override the model_aliases defined in
+#       .aipack/config.toml, ~/.aipack-base/config-user.toml, ~/.aipack-base/config-default.toml
 model_aliases:
-  gpro: gemini-2.5-pro
-  gpro-high: gemini-2.5-pro-high
-  gpro-low: gemini-2.5-pro-low
-  flash: gemini-2.5-flash
-  flash-zero: gemini-2.5-flash-zero
-  lite: gemini-2.5-flash-lite-preview-06-17
-  claude: claude-sonnet-4-20250514
-  gpt: gpt-4.1
-  mini: gpt-4.1-mini
+  my-model: gemini-2.5-pro
 
 # Typically, leave this commented for "search_replace_auto", which is the most efficient
 # file_content_mode: whole # default "search_replace_auto"
@@ -50,18 +42,20 @@ model_aliases:
 # Set to true to write the files (otherwise, they will be shown below the `====` separator)
 write_mode: false
 
-# It can be an alias name above, or model names like "o4-mini", "o4-mini-high".
-# If not set, the model defined in config.toml will be used.
-model: gpt
+# Here you can use any full model name or model aliases defined above and in the config.toml
+# such as ~/.aipack-base/config-default.toml
+
+model: gpt-4.1-mini
 
 # To see docs, type "Show Doc" and then press `r` in the aip terminal
 ```
 
 ====
-> Write your prompt above the `====` sign and below the parametric block (the YAML code block).
+
+> Write your prompt above the `====` line and below the parametric block (the YAML code block).
 > Press `r` in the terminal to replay this run; the AI response will appear below the `====`.
 > When `write_mode: true`, the file code block will be extracted and saved to the appropriate files.
-> You can ask pro@coder to give you some documentaiton by asking `Show Doc` and pres `r`
+> You can ask pro@coder to give you some documentation by asking `Show Doc` and pressing `r`
 ]]
 
 return {
