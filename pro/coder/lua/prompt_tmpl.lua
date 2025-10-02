0ad513aad37eed0209b44683c1cd0746961d1c08
Jeremy Chone <jeremy.chone@gmail.com>
2025-10-02 09:45:32 -0700

    - coder - fix issue about readme/plan file always saved in the default prompt dir (even when custom)

--- pro/coder/lua/prompt_tmpl.lua
+++ pro/coder/lua/prompt_tmpl.lua
@@ -2,37 +2,6 @@
 
 local TEMPLATES_DIR = CTX.AGENT_FILE_DIR .. "/templates"
 
--- ==== Support Functions
-
--- Save the For each template_files, if the file name is not int he dest_dir, will copy it
--- `dest_dir`       - e.g., .aipack/.prompt/pro@coder/dev/plan
--- `template_files` - e.g., 
--- 
--- NOTE: Support only one level at this time (and assume .md)
-function save_template_files(template_files, dest_dir)
-  -- Compute the existing files map by file.name
-  local existing_files_list = aip.file.list(dest_dir .. "/*.md")
-  local existing_files_map = {}
-  for _, f in ipairs(existing_files_list) do
-    existing_files_map[f.name] = true
-  end
-
-  -- For each template files, copy if not already there
-  for _, f in ipairs(template_files) do
-    if not existing_files_map[f.name] then
-      local content = aip.file.load(f.path).content
-      local dest_path = dest_dir .. "/" .. f.name
-      local dest_file = aip.file.save(dest_path, content)
-      print("dev plan file saved: " .. dest_path)
-    end
-  end
-
-end
-
-
-
--- ==== Public APIs
-
 -- Save a prompt file
 -- `prompt_path`    - the destination file path
 function save_prompt_file(prompt_path)
